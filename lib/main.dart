f9d7f3d23f398844b37458bf0536513e1e93eaef
sumitvarun <sumit.varun@rocketmail.com>
2021-12-18 00:44:46 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ecommerce App',
       theme: ThemeData(
+        fontFamily: 'MavenPro',
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
