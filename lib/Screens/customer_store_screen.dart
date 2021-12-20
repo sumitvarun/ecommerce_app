dbd74a6cf5a1a61e52316cd713be47395ad0dc13
sumitvarun <sumit.varun@rocketmail.com>
2021-12-20 11:34:15 +0530

    Update

--- lib/Screens/customer_store_screen.dart
+++ lib/Screens/customer_store_screen.dart
@@ -64,7 +64,131 @@ class _CustomerStoreState extends State<CustomerStore> {
             ),
           ],
         ),
-        body: Column(),
+        body: SafeArea(
+            child: GridView.count(
+          crossAxisCount: 1,
+          children: <Widget>[
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/fruits&vegetables.jpg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      'Ravi,s Veggie Corner',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      'Vegetables & Fruits',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/HEALTHY-COOKING-OILS.jpg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      'Patel Supermarket',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      'Beverages, staples, Dairy',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/meatfishegg.jpg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      'Meat & Fish Store',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      'Meat Fish chicken',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/bakery.jpg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      ' The Bakery Store',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      'Bakery & Snack',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/dairy.jpeg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      'Mother Dairy',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      'Milk Curd Ghee Yogurt Cheese Paneer ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Image.asset('lib/assets/images/beverages.jpg'),
+                  ListTile(
+                    //leading: const Icon(Icons.arrow_drop_down_circle),
+                    title: const Text(
+                      ' THE Beverages Store',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Text(
+                      ' ',
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
