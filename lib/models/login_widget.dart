a3887b2b69644e66c0e551f60dc4138ed98660d7
sumitvarun <sumit.varun@rocketmail.com>
2021-12-20 16:30:20 +0530

    Update

--- lib/models/login_widget.dart
+++ lib/models/login_widget.dart
@@ -1,3 +1,5 @@
+import 'package:country_code_picker/country_code.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:ecommerce_app/Screens/customer_store_screen.dart';
 import 'package:ecommerce_app/Screens/owner_store_screen.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +34,18 @@ class loginContainer extends StatelessWidget {
             ),
             const SizedBox(
               height: 16,
+            ),
+            CountryCodePicker(
+              onChanged: print,
+              // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+              initialSelection: 'In',
+              favorite: const ['+91', 'FR'],
+              // optional. Shows only country name and flag
+              showCountryOnly: false,
+              // optional. Shows only country name and flag when popup is closed.
+              showOnlyCountryWhenClosed: false,
+              // optional. aligns the flag and the Text left
+              alignLeft: false,
             ),
             TextFormField(
               decoration: const InputDecoration(
